911ff613aba8cde40c8012d1d9001549ffe6d3f8
Crash0v3r1de <nb.faulds@gmail.com>
2025-03-01 00:19:51 -0700

    added pending change bool

--- The Isle Evrima Manager/Threadz/ThreadTracking/GameServerStatusTracker.cs
+++ The Isle Evrima Manager/Threadz/ThreadTracking/GameServerStatusTracker.cs
@@ -13,5 +13,6 @@ namespace The_Isle_Evrima_Manager.Threadz.ThreadTracking
         public static bool AllowServerRunning = true; // Used for thread to know when to stop server
         public static bool ForceStop = false; // Used to force stop server
         public static int ServerPort = 7777; // Default port for The Isle
+        public static bool PendingSettingsChange = false; // Used to know when to apply new settings upon server stop/restart
     }
 }
